8c2e2074fecc42d9eba099364faa8466d643811e
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-09-01 13:59:04 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.defaults.baseURL =
+  'https://pixabay.com/api/?key=38328018-adf92d25e5f0a3816743083dd';
