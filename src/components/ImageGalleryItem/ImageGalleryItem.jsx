3274f1e1d9ec6c575bf89b913833d18ff060048c
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-09-04 02:50:25 +0300

    update

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -3,7 +3,7 @@ import { GalleryItem, GalleryImage } from './ImageGalleryItem.styled';
 export const ImageGalleryItem = ({ images }) => {
   return images.map(({ id, webformatURL }) => (
     <GalleryItem key={id}>
-      <GalleryImage src={webformatURL} alt={ id } />
+      <GalleryImage src={webformatURL} alt={id} />
     </GalleryItem>
   ));
 };
