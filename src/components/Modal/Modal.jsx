407add7a582859cada98cd3a576c333c93f46e59
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-09-05 11:56:20 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,20 +1,35 @@
-import { ModalReact, Image } from './Modal.styled';
+// import { ModalReact, Image } from './Modal.styled';
+import ReactModal from 'react-modal';
 
 export const Modal = ({ picture, onClose, isOpen }) => {
   const { largeImageURL, tags } = picture;
-  const customStyles = {
+  const styles = {
     overlay: {
-      zIndex: '1200',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      zIndex: '2',
+    },
+    content: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      maxWidth: 'calc(100vw - 48px)',
+      maxHeight: 'calc(100vh - 48px)',
+      border: 'none',
+      borderRadius: '16px',
+      position: 'revert',
+      overflow: 'hidden',
     },
   };
   return (
-    <ModalReact
+    <ReactModal
       isOpen={isOpen}
       onRequestClose={onClose}
-      style={customStyles}
+      style={styles}
       preventScroll={true}
     >
-      <Image src={largeImageURL} alt={tags} />
-    </ModalReact>
+      <img src={largeImageURL} alt={tags} />
+    </ReactModal>
   );
 };
